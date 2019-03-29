575c2460afc55dc5fa843857a7ca93fdc783f663
Katana Tran <tkatyk21@gmail.com>
2019-03-29 17:46:07 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,6 +1,4 @@
-age_of_person = current_age_for_birth_year(year)
-
-def current_age_for_birth_year(year)
+def current_age_for_birth_year(birth_year)
   age_of_person = 2019 - year
   puts "#{age_of_person}"
 end
